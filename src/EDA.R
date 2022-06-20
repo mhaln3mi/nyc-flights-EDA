e8b243426876792011c0b57de2015a87116a75bd
YousefAlotaibi <yousefotb@hotmail.com>
2022-06-20 17:02:52 +0300

    Checking for Duplicates and null values

--- src/EDA.R
+++ src/EDA.R
@@ -5,9 +5,18 @@ install.packages("nycflights13")
 # Initialize the libraries
 library(nycflights13)
 library(tidyverse)
-
+library(janitor)
 
 flights -> flights_df
+
+glimpse(flights_df)
+summary(flights_df)
+
+# Checking for duplicates.
+get_dupes(flights_df)
+
+# Checking for null values.
+is.null(flights_df)
 
 #TURKI
 
