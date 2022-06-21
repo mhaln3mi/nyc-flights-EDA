1eece02738a27853ac167b310fe72e2998383832
YousefAlotaibi <yousefotb@hotmail.com>
2022-06-21 18:10:05 +0300

    Couldn't plot dual bar plot for arr_delay and dep_delay, but instead i plotted each by itself could make better representing

--- src/EDA.R
+++ src/EDA.R
@@ -26,16 +26,45 @@ colSums(is.na(flights_df))
 
 #YOUSEF
 
+library("gridExtra")  
+
 dep_delay_cat <- cut(flights_df$dep_delay,
-                     breaks = c(-43,-1,0,30,120,1301),
+                     breaks = c(-45,-1,0,30,120,1301),
                      labels = c("Before Time",
                                 "On Time",
                                 "Low Delay (less than 30 min)",
                                 "Medium Delay (31-120 min)",
                                 "High Delay (more than 121 min)"))
 
-ggplot(flights_df, aes(dep_delay)) +
-  geom_point() 
- 
+arr_delay_cat <- cut(flights_df$arr_delay,
+                     breaks = c(-88,-1,0,30,120,1272),
+                     labels = c("Before Time",
+                                "On Time",
+                                "Low Delay (less than 30 min)",
+                                "Medium Delay (31-120 min)",
+                                "High Delay (more than 121 min)"))
+
+dep_plot <- ggplot(flights_df, aes(dep_delay_cat)) +
+  geom_bar() +
+  geom_text(aes(label= scales::percent(after_stat(as.double(prop))), group=1),
+            stat='count', vjust = -0.3,) +
+  scale_x_discrete(guide = guide_axis(n.dodge=2))+
+  labs(title="Percentage of NYC Flights Departure Delay (2013)",
+       x = "Departue Delay Time",
+       y = "Number of Departure Delays")
+
+
+arr_plot <- ggplot(flights_df, aes(arr_delay_cat)) +
+  geom_bar() +
+  geom_text(aes(label= scales::percent(after_stat(as.double(prop))), group=1),
+            stat='count', vjust = -0.3, ) +
+  scale_x_discrete(guide = guide_axis(n.dodge=2))+
+  labs(title="Percentage of NYC Flights Arrival Delay (2013)",
+       x = "Arrival Delay Time",
+       y = "Number of Arrival Delays")
+
+
+grid.arrange(dep_plot, arr_plot, ncol = 2)
+
 
 #SHAIMAA
