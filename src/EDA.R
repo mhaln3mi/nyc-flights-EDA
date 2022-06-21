32ccc0cbc4eb4940ff3d8274a16724fc0ff14103
SaadGhazai <saad.ghazzay@gmail.com>
2022-06-22 00:24:39 +0300

    did the corr metrices

--- src/EDA.R
+++ src/EDA.R
@@ -8,6 +8,7 @@ library(tidyverse)
 library(janitor)
 
 flights -> flights_df
+flights_df
 
 glimpse(flights_df)
 summary(flights_df)
@@ -23,6 +24,22 @@ is.null(flights_df)
 #MAAN
 
 #SAAD
+
+# What are the observations/attributes that influence dep_delay or arr_delay 
+# to be high or low? (mention scale/threshold used) 
+
+# We'll use the correlation matrix
+dep_df = subset(flights_df, 
+                select = c(dep_time, sched_dep_time, dep_delay, air_time, distance) )
+
+arr_df = subset(flights_df, 
+                select = c(arr_time, sched_arr_time, arr_delay, air_time, distance) )
+
+dep_matrix <- cor(dep_df, use = "complete.obs")
+dep_matrix
+
+arr_matrix <- cor(arr_df, use = "complete.obs")
+arr_matrix
 
 #YOSUF
 
