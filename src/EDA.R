7d9c44dd841587e889550af65b2087f87b632191
SaadGhazai <58564180+SaadGhazai@users.noreply.github.com>
2022-06-22 22:15:11 +0300

    Update EDA.R

--- src/EDA.R
+++ src/EDA.R
@@ -29,46 +29,16 @@ is.null(flights_df)
 # to be high or low? (mention scale/threshold used) 
 
 # We'll use the correlation matrix
-dep_df <- subset(flights_df, 
-                select = c(dep_time, sched_dep_time, dep_delay, air_time, distance) )
-
-arr_df <- subset(flights_df, 
-                select = c(arr_time, sched_arr_time, arr_delay, air_time, distance) )
-
-delays_df <- subset(flights_df, 
-                 select = c(dep_delay, arr_delay, air_time, distance) )
-
-time_df <- subset(flights_df, 
-                 select = c(dep_time, sched_dep_time,
-                            arr_time, sched_arr_time,
+all_df <- subset(flights_df, 
+                 select = c(dep_time, sched_dep_time, dep_delay,
+                            arr_time, sched_arr_time, arr_delay,
                             air_time, distance) )
 
-delays_matrix <- cor(delays_df, use = "complete.obs")
-delays_matrix
-
-time_matrix <- cor(time_df, use = "complete.obs")
-time_matrix
+all_matrix <- cor(all_df, use = "complete.obs")
 
 library(corrplot)
-
-corrplot(delays_matrix, type = "upper", order = "hclust", 
+corrplot(all_matrix, type = "upper", order = "hclust", 
          tl.col = "black", tl.srt = 45, diag = FALSE, method = "number")
-
-corrplot(time_matrix, type = "upper", order = "hclust", 
-         tl.col = "black", tl.srt = 45, diag = FALSE, method = "number")
-
-
-ggplot(flights_df, aes(distance, arr_delay)) +
-  geom_jitter()
-
-ggplot(flights_df, aes(distance, dep_delay)) +
-  geom_jitter()
-
-ggplot(flights_df, aes(air_time, arr_delay)) +
-  geom_jitter()
-
-ggplot(flights_df, aes(air_time, dep_delay)) +
-  geom_jitter()
 
 flights_df %>%
   slice_max(arr_delay) -> most_arr_delay
@@ -77,7 +47,7 @@ flights_df %>%
   slice_max(dep_delay) -> most_dep_delay
 
 glimpse(most_arr_delay)
-most_dep_delay
+glimpse(most_dep_delay)
 
 #YOSUF
 
